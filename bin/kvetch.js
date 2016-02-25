2cf521db2c9e694db09a9e70524314b41ff46e33
Aaron Leventhal <aleventhal@aisquared.com>
2016-02-25 10:26:54 -0500

    Restructured and using Promise.all

--- bin/kvetch.js
+++ bin/kvetch.js
@@ -1,6 +1,17 @@
 'use strict';
 
 const config = require('./cli-config.js'),
-  checker = require('../lib/checker.js');
+  checker = require('../lib/checker.js'),
+  fs = require('fs');
 
-checker(config);
+checker(config) // No need to catch errors for now -- just let exception through
+  .then(function(rawReport) {
+    console.log('\n\nWriting report ...');
+    if (config.outFile) {
+      fs.writeFileSync(config.outFile, rawReport);
+    }
+    else {
+      console.log(rawReport);
+    }
+    console.log('Finished'); 
+  });
